ad1682d49a3bec00e1d25a2f7c367048eb55ccab
Rishant Dutt <rishant.dutt96@gmail.com>
2019-02-04 12:36:43 +0530

    On branch master
    Initial commit
    
    Changes to be committed:
    	new file:   .gitignore
    	new file:   placeholder/placeholder.py
    	new file:   project_name/project_name.py

---
+++ placeholder/placeholder.py
@@ -0,0 +1,89 @@
+import os
+import sys
+
+from django.conf import settings
+
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '$96jugz_+vnli0++um#6)ndrb7-a)1ex=uai9n95ppa6&zq&zv')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+settings.configure(
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+from django.conf.urls import url
+from django.core.cache import cache
+from django import forms
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse, HttpResponseBadRequest
+
+from io import  BytesIO
+from PIL import Image, ImageDraw
+
+
+class ImageForm(forms.Form):
+    """Form to validate requested placeholder image."""
+
+    height = forms.IntegerField(min_value=1, max_value=2000)
+    width = forms.IntegerField(min_value=1, max_value=2000)
+    
+    def generate( self, image_format='PNG' ):
+        """Generate an image of the given type and return as raw bytes."""
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+
+        key = f'{width}.{height}.{image_format}'
+        content = cache.get(key)
+
+        if content is None:
+            image = Image.new('RGB', (width, height))
+            draw = ImageDraw.Draw(image)
+            text = f'{width} x {height}'
+            textwidth, textheight = draw.textsize(text)
+            
+            if textwidth < width and textheight < height:
+                texttop = (height - textheight)
+                textleft = (width - textwidth)
+                draw.text((textleft, texttop), text, fill=(255,255,255))
+                
+            content = BytesIO()
+            image.save(content, image_format)
+            content.seek(0)
+            cache.set(key, content, 60 * 60)
+
+        return content        
+
+def placeholder( request, width, height ):
+    form = ImageForm({'height': height, 'width': width})
+    if form.is_valid():
+        image = form.generate()
+        return HttpResponse(image, content_type='image/png')
+    else:
+        return HttpResponseBadRequest('Invalid image request')
+
+def index( request ):
+    return HttpResponse('Hello World')
+
+urlpatterns = (
+    url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder,
+        name='placeholder'),
+    url(r'^$', index, name='homepage'),
+)
+
+application = get_wsgi_application()
+
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
