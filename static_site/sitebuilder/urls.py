a26863d3ed10e7e053426301151673faf7655850
Rishant Dutt <rishant.dutt96@gmail.com>
2019-02-07 10:42:18 +0530

    dependencies and some styling added.

--- static_site/sitebuilder/urls.py
+++ static_site/sitebuilder/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .views import page
 
 urlpatterns = (
-    url(r'^(?P<slug>[\w./-]+)/$', page, name='page'),
+    url(r'^(?P<slug>[\w.-/]+)/$', page, name='page'),
     url(r'^$', page, name='homepage'),
 )
